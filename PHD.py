ea906da190be6cc7acb88b47de65e19d3924208f
naraindyah34 <149661231+naraindyah34@users.noreply.github.com>
2023-11-02 17:22:39 +0700

    Update PHD.py

--- PHD.py
+++ PHD.py
@@ -19,3 +19,23 @@ if MenuPizza in ["frankfurter bbq", "meat monsta", "super supreme", "super supre
 else:
     hargaPizza = 0
     print("Maaf, Menu pizza yang anda pilih tidak tersedia")
+
+# memilih crust pizza
+print("Pilihan Crust: ")
+print("Pan")
+print("StuffedCrust Cheese")
+print("StuffedCrust Sausage")
+print("Chessy Bites")
+CrustPizza = input("Silahkan pilih crust pizza : ")
+CrustPizza = CrustPizza.lower()
+if CrustPizza == "original":
+    hargaPizza += 0
+elif CrustPizza == "stuffedcrust cheese":
+    hargaPizza += 11819
+elif CrustPizza == "stuffedcrust sausage":
+    hargaPizza += 9091
+elif CrustPizza == "chessy bites":
+    hargaPizza += 13637
+else:
+    hargaPizza += 0
+    print("Maaf, Crust yang anda pilih tidak tersedia")
